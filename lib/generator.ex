ae47cae962a1f21d7cb7136cd4b8f6beeafb5c1d
tim <tim@imac-tim.lan>
2016-10-07 20:00:00 +0300

    dynamically change algorithms

--- lib/generator.ex
+++ lib/generator.ex
@@ -2,6 +2,8 @@ defmodule Erlng.Generator do
 	use GenServer
 	require Logger
 	@seed_limit 10
+	@algorithms [:exsplus, :exs64, :exs1024]
+	@algorithms_len length(@algorithms)
 
 	#
 	#	public
@@ -12,7 +14,7 @@ defmodule Erlng.Generator do
 
 	def handle_call(cmd, from, 0) do
 		_ = seed()
-		handle_call(cmd, from, @seed_limit)
+		handle_call(cmd, from, (:rand.uniform(@seed_limit) * 2))
 	end
 	def handle_call(:uniform, _, count), do: {:reply, :rand.uniform, count-1}
 	def handle_call({:uniform, max}, _, count), do: {:reply, :rand.uniform(max), count-1}
@@ -31,7 +33,7 @@ defmodule Erlng.Generator do
 
 	defp seed() do
 		<<a :: 32, b :: 32, c :: 32>> = :crypto.strong_rand_bytes(12)
-		_ = :rand.seed(:exs1024, {a,b,c})
+		_ = Enum.at(@algorithms, (:rand.uniform(@algorithms_len) - 1)) |> :rand.seed({a,b,c})
 		_ = Logger.info("#{__MODULE__} seed #{a}, #{b}, #{c}")
 	end
 
