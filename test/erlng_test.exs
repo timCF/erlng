d3aba1544bf5e124fcd3815767e13e666c5585e5
tim <tim@imac-tim.lan>
2016-10-07 19:29:55 +0300

    add tests

--- test/erlng_test.exs
+++ test/erlng_test.exs
@@ -1,12 +1,17 @@
 defmodule ErlngTest do
   use ExUnit.Case
+  require Logger
   doctest Erlng
+  @roulacc Enum.reduce(0..36, %{}, &(Map.put(&2,&1,0)))
 
   test "the truth" do
-    Erlng.uniform() |> IO.inspect
-    Erlng.uniform(10) |> IO.inspect
-    Erlng.shuffle(1..10) |> IO.inspect
-    Erlng.range(1, 10) |> IO.inspect
+    Erlng.uniform() |> Logger.info
+    Erlng.uniform(10) |> Logger.info
+    Erlng.shuffle(1..10) |> Logger.info
+    Erlng.range(1, 10) |> Logger.info
+    Enum.reduce(1..1000000, @roulacc, fn(_, acc) -> Map.update!(acc, Erlng.range(0,36), &(&1+1)) end)
+    |> Enum.sort(fn({_,v1},{_,v2}) -> v1 < v2 end)
+    |> Enum.each(fn({k,v}) -> Logger.info("#{k} => #{v}") end)
     assert true
   end
 end
